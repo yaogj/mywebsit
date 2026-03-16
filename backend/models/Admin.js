85ea58b53d761680ec7f8c42d1e6058d3c3a639b
yaogj <yaoguijing-04@126.com>
2026-03-16 20:55:53 +0800

    Add admin user schema with password hashing

---
+++ backend/models/Admin.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const adminUserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+// Password hashing
+adminUserSchema.pre('save', async function(next) {
+    if (this.isModified('password') || this.isNew) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
+
+const AdminUser = mongoose.model('AdminUser', adminUserSchema);
+
+module.exports = AdminUser;
